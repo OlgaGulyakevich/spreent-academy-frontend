49e004980a40015201fe27278613ba5b869eb9f7
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2026-04-13 22:08:36 +0200

    fix: enable :active pseudo-class on iOS Safari by adding a passive touchstart listener

--- source/js/main.js
+++ source/js/main.js
@@ -11,6 +11,10 @@ import { initScrollProgress } from './modules/scroll-progress.js';
 import { initScrollReveal } from './modules/scroll-reveal.js';
 import { initSmoothScroll } from './modules/smooth-scroll.js';
 
+// Enable :active pseudo-class on iOS Safari
+// Without this listener, Safari may not trigger :active reliably after the first tap
+document.addEventListener('touchstart', () => {}, true);
+
 // Prevent scroll-to-top on placeholder links
 document.addEventListener('click', (e) => {
   if (e.target.closest('a[href="#"]')) {
