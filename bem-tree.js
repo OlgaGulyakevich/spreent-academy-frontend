18a9cbfb13ba3de68ddc6b6c20ada59550d20159
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2026-07-20 15:34:32 +0200

    refactor: migrate source assets and scripts to public and src directories for project structure optimization

--- bem-tree.js
+++
@@ -1,8 +0,0 @@
-import bemlinter from 'gulp-html-bemlinter';
-import vfs from 'vinyl-fs';
-
-vfs
-  .src(['./source/**/*.html', '!./source/**/vendor/*.html'])
-  .pipe(bemlinter())
-  .on('end', () => process.exit(0))
-  .on('error', () => process.exit(1));
